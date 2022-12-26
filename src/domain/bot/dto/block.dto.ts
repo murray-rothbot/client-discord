ead4bbf4b181efdf38b84be3f7f0056f6e68b151
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2022-12-26 01:44:10 -0300

    fix: remove commentaries

--- src/domain/bot/dto/block.dto.ts
+++ src/domain/bot/dto/block.dto.ts
@@ -1,9 +1,6 @@
 import { Param } from '@discord-nestjs/core'
-// import { Transform } from 'class-transformer'
-// import { Transform } from 'class-transformer'
 
 export class BlockDto {
-  // @Transform(({ value }) => value.toUpperCase())
   @Param({
     name: 'id',
     description: 'Block hash or block height.',
