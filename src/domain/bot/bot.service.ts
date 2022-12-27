dffc1734a03ad670eb2e145032b7298876a58225
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2022-12-27 14:08:57 -0300

    feat: add mempool websocket (#35)

--- src/domain/bot/bot.service.ts
+++ src/domain/bot/bot.service.ts
@@ -4,27 +4,88 @@ import { Client } from 'discord.js'
 import { Cron } from '@nestjs/schedule'
 import { PricesServiceRepository } from './repositories/pricesservice.repository'
 import { NumbersService } from 'src/utils/numbers/numbers.service'
+import {
+  InjectWebSocketProvider,
+  WebSocketClient,
+  OnOpen,
+  OnClose,
+  OnMessage,
+} from 'nestjs-websocket'
 
 @Injectable()
 export class BotService {
   private readonly logger = new Logger(BotService.name)
+  private data: Record<any, any> = {}
+  private wsConnected = false
 
   constructor(
+    @InjectWebSocketProvider()
+    private readonly ws: WebSocketClient,
     @InjectDiscordClient()
     private readonly client: Client,
     private readonly pricesRepository: PricesServiceRepository,
     private readonly numbersService: NumbersService,
   ) {}
 
-  @Once('ready')
-  onReady() {
-    this.logger.log(`Bot ${this.client.user.tag} was started!`)
-    this.onCron()
+  @OnOpen()
+  openWs() {
+    try {
+      this.logger.log(`Mempool.Space Websocket is established.`)
+      this.ws.send(
+        JSON.stringify({
+          action: 'want',
+          data: ['blocks', 'fees'],
+        }),
+      )
+      this.pingWs()
+      this.wsConnected = true
+    } catch (error) {
+      this.wsConnected = false
+    }
   }
 
-  // cronjob example
-  @Cron('*/5 * * * * *')
-  async onCron() {
+  @OnClose()
+  closeWs() {
+    this.wsConnected = false
+  }
+
+  pingWs() {
+    this.ws.send(
+      JSON.stringify({
+        action: 'ping',
+      }),
+    )
+  }
+
+  @Cron('*/10 * * * * *')
+  checkWs() {
+    this.pingWs()
+    if (this.wsConnected) return
+    this.logger.error(`Mempool.Space Websocket is disconnected.`)
+    this.openWs()
+  }
+
+  @OnMessage()
+  messageWs(data: WebSocketClient.Data) {
+    this.data = JSON.parse(data.toString())
+    if (this.data.block) {
+      this.client.user.setActivity(`New Block: ${this.data.block.height}`)
+    }
+    if (this.data.pong) {
+      this.logger.debug(`Mempool.Space Websocket pong.`)
+    }
+  }
+
+  @Once('ready')
+  async onReady() {
+    this.logger.log(`Bot ${this.client.user.tag} was started!`)
+    this.openWs()
+    this.updatePresence()
+  }
+
+  @Cron('*/10 * * * * *')
+  async updatePresence() {
+    this.logger.debug(`Update presence Tickers.`)
     const symbols = ['BTCUSD', 'BTCBRL']
     const promises = symbols.map(async (symbol) => {
       return await this.pricesRepository.getTicker({ symbol })
@@ -46,14 +107,19 @@ export class BotService {
       }
     })
 
-    const priceChangeUSD = priceChangePercentUsd <= 0 ? '▼' : '▲'
-    const priceChangeBRL = priceChangePercentBrl <= 0 ? '▼' : '▲'
-    const msg = `${priceChangeUSD}$${this.numbersService.kFormatter(
-      lastPriceUsd,
-    )} ${priceChangeBRL}R$${this.numbersService.kFormatter(lastPriceBrl)}`
-    const status = priceChangePercentUsd <= 0 ? 'dnd' : 'online'
+    // check if both prices are available
+    if (lastPriceUsd > 0 && lastPriceBrl > 0) {
+      const priceChangeUSD = priceChangePercentUsd <= 0 ? '▼' : '▲'
+      const priceChangeBRL = priceChangePercentBrl <= 0 ? '▼' : '▲'
+      const msg = `${priceChangeUSD}$${this.numbersService.kFormatter(
+        lastPriceUsd,
+      )} ${priceChangeBRL}R$${this.numbersService.kFormatter(lastPriceBrl)}`
+      const status = priceChangePercentUsd <= 0 ? 'dnd' : 'online'
 
-    this.client.user.setStatus(status)
-    this.client.user.setActivity(msg)
+      this.client.user.setStatus(status)
+      this.client.user.setActivity(msg)
+    } else {
+      this.logger.error(`Update presence Tickers failed.`)
+    }
   }
 }
