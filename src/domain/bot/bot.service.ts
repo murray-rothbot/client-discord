40fdd90788af476fb47f99058ba29a54709f6bcc
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2023-02-04 22:45:31 -0300

    feat: add cronjobs webhooks

--- src/domain/bot/bot.service.ts
+++ src/domain/bot/bot.service.ts
@@ -1,5 +1,34 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { MurrayServiceRepository } from './repositories'
 @Injectable()
 export class BotService {
-  constructor() {}
+  constructor(
+    private readonly murrayService: MurrayServiceRepository,
+    private readonly cfgService: ConfigService,
+  ) {
+    this.addCronjobList()
+  }
+
+  webhookUrl: string = this.cfgService.get<string>('CLIENT_DISCORD_WEBHOOK')
+
+  // add cronjob list
+  addCronjobList() {
+    // cronjob list
+    const cronjobList = [
+      {
+        webhook: `${this.webhookUrl}/new-price`,
+        interval: '*/20 * * * * *',
+        type: 'price',
+      },
+      {
+        webhook: `${this.webhookUrl}/new-block`,
+        interval: '* * * * * *',
+        type: 'block',
+      },
+    ]
+
+    // add cronjob list
+    this.murrayService.addCronJobs(cronjobList)
+  }
 }
