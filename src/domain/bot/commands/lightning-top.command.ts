c01480d78e5ae2814134c3c10414e29eee174151
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-02-10 22:13:55 -0300

    fix: remove unnecessary code

--- src/domain/bot/commands/lightning-top.command.ts
+++ src/domain/bot/commands/lightning-top.command.ts
@@ -16,38 +16,5 @@ export class LightningTopCommand implements DiscordCommand {
   async handler(interaction: CommandInteraction): Promise<{}> {
     const lnStatsTop = await this.murrayRepository.getLightingTop()
     return createResponse(lnStatsTop.data)
-
-    // const response = defaultResponse()
-    // const embed = response.embeds[0]
-    // const fields = embed.fields
-
-    // embed.title = '⚡ Top Lightning Nodes'
-
-    // const { data } = await this.lightningRepository.getTopNodes()
-
-    // const byCapacity = data['topByCapacity']
-    // const byChannels = data['topByChannels']
-
-    // const medals = ['🥇', '🥈', '🥉']
-
-    // fields.push({ name: '\u200B', value: '_By capacity:_' })
-    // for (let i = 0; i < 3; i++) {
-    //   fields.push({
-    //     name: `${medals[i]} ${byCapacity[i]['alias']}`,
-    //     value: `${this.numbersService.formatterSATS.format(byCapacity[i]['capacity'])} sats`,
-    //     inline: false,
-    //   })
-    // }
-
-    // fields.push({ name: '\u200B', value: '_By channels count:_' })
-    // for (let i = 0; i < 3; i++) {
-    //   fields.push({
-    //     name: `${medals[i]} ${byChannels[i]['alias']}`,
-    //     value: `${this.numbersService.formatterSATS.format(byChannels[i]['channels'])} channels`,
-    //     inline: false,
-    //   })
-    // }
-
-    // return response
   }
 }
